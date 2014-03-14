5cede24d860113d425bc71c0c844473ca2899f8c
Franco M. Luque <francolq@gmail.com>
2014-03-14 15:21:02 -0300

    First version of stanford tagger downloading.

--- iepy/tagger.py
+++ iepy/tagger.py
@@ -1,11 +1,14 @@
+import urllib2
+import os
+import os.path
 from nltk.tag.stanford import POSTagger
 
 from iepy.models import PreProcessSteps
 from iepy.preprocess import BasePreProcessStepRunner
 
 
-stanford_tagger_path = '../stanford-postagger-2014-01-04/'
-stanford_ner_path = '../stanford-ner-2014-01-04/'
+stanford_tagger_name = 'stanford-postagger-2014-01-04'
+download_url_base = 'http://nlp.stanford.edu/software/'
 
 
 class StanfordTaggerRunner(BasePreProcessStepRunner):
@@ -16,10 +19,17 @@ class StanfordTaggerRunner(BasePreProcessStepRunner):
     def __init__(self, override=False):
         """override:
         """
+        # check installation:
+        if not os.path.exists(stanford_tagger_name):
+            print 'Downloading Stanford POS tagger...'
+            package_filename = '{0}.zip'.format(stanford_tagger_name)
+            download(download_url_base + package_filename, package_filename)
+            os.system('unzip {0}'.format(package_filename))
+        
         self.override = override
-        self.pos_tagger = POSTagger(stanford_tagger_path + 
-                                'models/english-bidirectional-distsim.tagger', 
-                                stanford_tagger_path + 'stanford-postagger.jar')
+        self.pos_tagger = POSTagger(stanford_tagger_name + 
+                                '/models/english-bidirectional-distsim.tagger', 
+                                stanford_tagger_name + '/stanford-postagger.jar')
 
     def __call__(self, doc):
         if not doc.was_preprocess_done(PreProcessSteps.segmentation):
@@ -35,4 +45,14 @@ class StanfordTaggerRunner(BasePreProcessStepRunner):
             
         doc.set_preprocess_result(PreProcessSteps.tagging, tagged_doc)
         doc.save()
+
+
+def download(url, filename):
+    infile = urllib2.urlopen(url)
+    meta = infile.info()
+    print 'File size: {0} bytes.'.format(int(meta.getheaders("Content-Length")[0]))
+    outfile = open(filename, 'w')
+    outfile.write(infile.read())
+    infile.close()
+    outfile.close()
 
