ff2748caae10ef5ef37ec6789809148d926ca85b
Franco M. Luque <francolq@gmail.com>
2014-03-14 18:37:24 -0300

    Factor out downloading. Add generic class TaggerRunner and refactor a bit.

--- iepy/tagger.py
+++ iepy/tagger.py
@@ -1,4 +1,4 @@
-import urllib2
+import urllib
 import os
 import os.path
 from nltk.tag.stanford import POSTagger
@@ -7,32 +7,23 @@ from iepy.models import PreProcessSteps
 from iepy.preprocess import BasePreProcessStepRunner
 
 
-stanford_tagger_name = 'stanford-postagger-2014-01-04'
+stanford_postagger_name = 'stanford-postagger-2014-01-04'
 download_url_base = 'http://nlp.stanford.edu/software/'
 
 
-class StanfordTaggerRunner(BasePreProcessStepRunner):
+class TaggerRunner(BasePreProcessStepRunner):
     """Wrapper to insert a generic callable sentence POS tagger into the pipeline.
     """
     step = PreProcessSteps.tagging
     
-    def __init__(self, override=False):
+    def __init__(self, postagger, override=False):
         """override:
         """
-        # check installation:
-        if not os.path.exists(stanford_tagger_name):
-            print 'Downloading Stanford POS tagger...'
-            package_filename = '{0}.zip'.format(stanford_tagger_name)
-            download(download_url_base + package_filename, package_filename)
-            os.system('unzip {0}'.format(package_filename))
-        
+        self.postagger = postagger
         self.override = override
-        self.pos_tagger = POSTagger(stanford_tagger_name + 
-                                '/models/english-bidirectional-distsim.tagger', 
-                                stanford_tagger_name + '/stanford-postagger.jar')
-
+        
     def __call__(self, doc):
-        if not doc.was_preprocess_done(PreProcessSteps.segmentation):
+        if not doc.was_preprocess_done(PreProcessSteps.sentencer):
             return
         if not self.override and doc.was_preprocess_done(PreProcessSteps.tagging):
             #print 'Already done'
@@ -40,19 +31,32 @@ class StanfordTaggerRunner(BasePreProcessStepRunner):
             
         tagged_doc = []
         for sent in doc.get_sentences():
-            tagged_sent = self.pos_tagger.tag(sent)
+            tagged_sent = self.postagger.tag(sent)
             tagged_doc.extend([tag for token, tag in tagged_sent])
             
         doc.set_preprocess_result(PreProcessSteps.tagging, tagged_doc)
         doc.save()
 
 
-def download(url, filename):
-    infile = urllib2.urlopen(url)
-    meta = infile.info()
-    print 'File size: {0} bytes.'.format(int(meta.getheaders("Content-Length")[0]))
-    outfile = open(filename, 'w')
-    outfile.write(infile.read())
-    infile.close()
-    outfile.close()
+class StanfordTaggerRunner(TaggerRunner):
+
+    def __init__(self, override=False):
+        if not os.path.exists(stanford_postagger_name):
+            raise LookupError("Stanford POS tagger not found.")
+        
+        postagger = POSTagger(stanford_postagger_name + 
+                            '/models/english-bidirectional-distsim.tagger', 
+                            stanford_postagger_name + '/stanford-postagger.jar')
+        
+        TaggerRunner.__init__(self, postagger, override)
+
+
+def download():
+    print 'Downloading Stanford POS tagger...'
+    package_filename = '{0}.zip'.format(stanford_postagger_name)
+    #print 'Downloaded {0:3}%'.format(blocks * block_size * 100 / total),
+    #def hook(blocks, block_size, total):
+    #    print 'Downloaded {0:3}%'.format(blocks * block_size * 100 / total),
+    urllib.urlretrieve(download_url_base + package_filename, package_filename)
+    os.system('unzip {0}'.format(package_filename))
 
