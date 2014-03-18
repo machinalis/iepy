62cc8077b9515c9a6a2622a8ca77c3d1038877e4
Javier Mansilla <jmansilla@machinalis.com>
2014-03-18 10:33:48 -0300

    implemented fix and tests added

--- tvseries/scripts/preprocess.py
+++ tvseries/scripts/preprocess.py
@@ -5,6 +5,8 @@ Usage:
     preprocess.py <dbname>
 
 """
+import re
+
 from docopt import docopt
 from mwtextextractor import get_body_text
 
@@ -14,8 +16,12 @@ from iepy.tokenizer import TokenizeSentencerRunner
 
 
 def media_wiki_to_txt(doc):
-    if not doc.text and doc.metadata['raw_text']:
-        doc.text = get_body_text(doc.metadata['raw_text'])
+    if not doc.text and doc.metadata.get('raw_text', ''):
+        # After MW strip, titles will not be recognizable. If they dont
+        # with a dot, will be very hard to split in sentences correctly.
+        raw = doc.metadata['raw_text']
+        raw = re.subn(r'==(.*)==', r'==\1.==', raw)[0]
+        doc.text = get_body_text(raw)
         doc.save()
 
 if __name__ == '__main__':
