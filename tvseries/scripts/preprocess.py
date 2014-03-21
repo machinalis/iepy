6203c5ab1dfd68867c1e307737d1d2c0d9327e42
Daniel Moisset <dmoisset@machinalis.com>
2014-03-21 12:01:02 -0300

    Integrated remaining preprocess steps

--- tvseries/scripts/preprocess.py
+++ tvseries/scripts/preprocess.py
@@ -13,7 +13,13 @@ from mwtextextractor import get_body_text
 from iepy.db import connect, DocumentManager
 from iepy.preprocess import PreProcessPipeline
 from iepy.tokenizer import TokenizeSentencerRunner
+<<<<<<< HEAD
 from iepy.lit_tagger import LitTaggerRunner
+=======
+from iepy.tagger import StanfordTaggerRunner
+from iepy.ner import StanfordNERRunner
+from iepy.segmenter import SyntacticSegmenterRunner
+>>>>>>> Integrated remaining preprocess steps
 
 
 def media_wiki_to_txt(doc):
@@ -42,10 +48,14 @@ if __name__ == '__main__':
     opts = docopt(__doc__, version=0.1)
     connect(opts['<dbname>'])
     docs = DocumentManager()
-    pipeline = PreProcessPipeline(
-                        [media_wiki_to_txt, 
-                        TokenizeSentencerRunner(), 
-                        TVSeriesNERRunner()], 
-                    docs)
+    pipeline = PreProcessPipeline([
+            media_wiki_to_txt,
+            TokenizeSentencerRunner(),
+            StanfordTaggerRunner(),
+            TVSeriesNERRunner(),
+            StanfordNERRunner(),
+            SyntacticSegmenterRunner(),
+        ], docs
+    )
     pipeline.process_everything()
-    
+
