300bc2d7a17d918433d8ead1a9f02ae3a4ae3ce4
Javier Mansilla <jmansilla@machinalis.com>
2014-04-01 18:30:50 -0300

    moved csv interaction to utils

--- iepy/utils.py
+++ iepy/utils.py
@@ -1,7 +1,12 @@
+import codecs
+from csv import reader
 from getpass import getuser
 import zipfile
 
 from appdirs import AppDirs
+
+from iepy.core import Fact
+from iepy import db
 
 DIRS = AppDirs('iepy', getuser())
 
@@ -25,3 +30,21 @@ def unzip(zipped_list, n):
 def unzip_file(zip_path, extraction_base_path):
     zfile = zipfile.ZipFile(zip_path)
     zfile.extractall(extraction_base_path)
+
+
+def load_facts_from_csv(filepath):
+    """Returns an iterable of facts from a CSV file encoded in UTF-8.
+    It's assumend that first 4 columns are
+        entity a kind, entity a key, entity b kind, entity b key
+    and that the 5th column is the relation name.
+    Everything else in the file will be ignored.
+    Row with less column than stated, will be ignored.
+    """
+    with codecs.open(filepath, mode='r', encoding='utf-8') as csvfile:
+        factsreader = reader(csvfile, delimiter=',')
+        for row in factsreader:
+            if len(row) < 5:
+                continue
+            entity_a = db.get_entity(row[0], row[1])
+            entity_b = db.get_entity(row[2], row[3])
+            yield Fact(entity_a, row[4], entity_b)
