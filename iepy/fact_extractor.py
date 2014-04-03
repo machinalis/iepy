8d2e4416a5c03c6ce4bd5aaee3c242a09189ae9b
Javier Mansilla <jmansilla@machinalis.com>
2014-04-03 18:05:32 -0300

    Defined simplistict FactExtractorFactory that interfaces fine with sklearn classifiers

--- iepy/fact_extractor.py
+++ iepy/fact_extractor.py
@@ -1,7 +1,22 @@
+from featureforge.vectorizer import Vectorizer
+from sklearn.pipeline import Pipeline
 from sklearn.tree import DecisionTreeRegressor
 
 
-def FactExtractorFactory():
-    """Instantiates a classifier ready to be fit"""
-    # For now, just return something
-    return DecisionTreeRegressor()
+def FactExtractorFactory(data, relation):
+    """Instantiates and trains a classifier."""
+    # Simplistic Pseudocode. For now, just return some dummy classifier
+    p = Pipeline([
+        ('vectorizer', get_vectorizer(relation)),
+        ('classifier', DecisionTreeRegressor())
+    ])
+    X, y = zip(*data.items())
+    p.fit_transform(X, y)
+    return p
+
+
+def get_vectorizer(relation):
+    # Pseudocode. Example Feature.
+    def _relation_name_in_segment(relation_name, evidence):
+        return relation_name.lower() in map(lambda x: x.lower(), evidence.segment.tokens)
+    return Vectorizer([lambda e: _relation_name_in_segment(relation, e)])
