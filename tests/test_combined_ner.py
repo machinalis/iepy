696de7ef821742a81fef0c451249242e9044bd8c
Franco M. Luque <francolq@gmail.com>
2014-04-04 15:48:30 -0300

    One more test for NER combiner.

--- tests/test_combined_ner.py
+++ tests/test_combined_ner.py
@@ -6,6 +6,7 @@ except ImportError:
 from unittest import TestCase
 
 from iepy.combined_ner import CombinedNERRunner
+from iepy.models import PreProcessSteps
 
 
 class TestCombinedNERRunner(TestCase):
@@ -14,34 +15,51 @@ class TestCombinedNERRunner(TestCase):
         self.runner1 = mock.MagicMock()
         self.runner2 = mock.MagicMock()
         self.doc = mock.MagicMock()
-    
+        self.doc.was_preprocess_done.side_effect = lambda x: False
+
     def test_runners_called_when_not_done_before(self):
         runner1, runner2, doc = self.runner1, self.runner2, self.doc
-        doc.was_preprocess_done.side_effect = lambda x: False
-        
+
         runner = CombinedNERRunner(runner1, runner2)
         runner(doc)
-        
+
         runner1.assert_called_once_with(doc)
         runner2.assert_called_once_with(doc)
 
     def test_runners_called_when_override(self):
         runner1, runner2, doc = self.runner1, self.runner2, self.doc
         doc.was_preprocess_done.side_effect = lambda x: True
-        
+
         runner = CombinedNERRunner(runner1, runner2, override=True)
         runner(doc)
-        
+
         runner1.assert_called_once_with(doc)
         runner2.assert_called_once_with(doc)
 
     def test_runners_not_called_when_done_before(self):
         runner1, runner2, doc = self.runner1, self.runner2, self.doc
         doc.was_preprocess_done.side_effect = lambda x: True
+
+        runner = CombinedNERRunner(runner1, runner2)
+        runner(doc)
+
+        self.assertFalse(runner1.called)
+        self.assertFalse(runner2.called)
+
+    def test_no_entities_are_lost(self):
+        runner1, runner2, doc = self.runner1, self.runner2, self.doc
+        
+        def set_result(doc, entities):
+            doc.get_preprocess_result.side_effect = lambda x: entities
+        
+        e1 = mock.MagicMock()
+        e1.offset = 1
+        e2 = mock.MagicMock()
+        e2.offset = 2
+        runner1.side_effect = lambda doc: set_result(doc, [e1])
+        runner2.side_effect = lambda doc: set_result(doc, [e2])
         
         runner = CombinedNERRunner(runner1, runner2)
         runner(doc)
-        
-        self.assertFalse(runner1.called)
-        self.assertFalse(runner2.called)
+        doc.set_preprocess_result.assert_called_once_with(PreProcessSteps.nerc, [e1, e2])
 
