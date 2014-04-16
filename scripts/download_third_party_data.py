5bae09c472e0e8997e06fdf1204665a1b0f6a51c
Javier Mansilla <jmansilla@machinalis.com>
2014-04-16 09:34:47 -0300

    downloading wordnet data from nltk

--- scripts/download_third_party_data.py
+++ scripts/download_third_party_data.py
@@ -1,16 +1,13 @@
-import os
-
 import nltk.data
 from iepy.tagger import download as download_tagger
 from iepy.ner import download as download_ner
-from iepy.utils import DIRS
 
 
 def download_third_party_data():
     print("Downloading punkt tokenizer")
-    if not os.path.exists(DIRS.user_data_dir):
-        os.mkdir(DIRS.user_data_dir)
-    nltk.download("punkt", DIRS.user_data_dir)
+    nltk.download("punkt")
+    print("Downloading wordnet")
+    nltk.download("wordnet")
     download_tagger()
     download_ner()
 
