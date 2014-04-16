ace0d80e8165b43310befe0b5f384f290fa96c0e
Franco M. Luque <francolq@gmail.com>
2014-04-16 15:26:38 -0300

    Use preprocess script template in startapp.

--- scripts/startapp.py
+++ scripts/startapp.py
@@ -5,9 +5,11 @@ Usage:
     startapp.py <name>
 """
 import os
+import shutil
 
 from docopt import docopt
 
+import iepy
 
 def startapp(name):
     # Sanitize name:
@@ -21,10 +23,14 @@ def startapp(name):
     os.mkdir(folder)
     os.mkdir(os.path.join(folder, 'scripts'))
     os.mkdir(os.path.join(folder, 'tests'))
-
-    # Create scripts
     os.mknod(os.path.join(folder, 'scripts', '__init__.py'))
     os.mknod(os.path.join(folder, 'tests', '__init__.py'))
+
+    # Create preprocess script
+    iepy_basedir = os.path.dirname(os.path.abspath(iepy.__file__))
+    template_filename = os.path.join(iepy_basedir, 'app_preprocess.py.template')
+    preprocess_filename = os.path.join(folder, 'scripts', 'preprocess.py') 
+    shutil.copyfile(template_filename, preprocess_filename)
 
 
 if __name__ == '__main__':
