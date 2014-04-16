917a38845f3fd03805b99c3a8df6f09b047ed0e0
Franco M. Luque <francolq@gmail.com>
2014-04-16 14:27:32 -0300

    Fix tvseries preprocess script to accomodate literal NER refactor.

--- examples/tvseries/scripts/preprocess.py
+++ examples/tvseries/scripts/preprocess.py
@@ -14,7 +14,7 @@ from iepy.db import connect, DocumentManager
 from iepy.models import set_custom_entity_kinds
 from iepy.preprocess import PreProcessPipeline
 from iepy.tokenizer import TokenizeSentencerRunner
-from iepy.lit_tagger import LitTaggerRunner
+from iepy.lit_tagger import LiteralNERRunner
 from iepy.tagger import StanfordTaggerRunner
 from iepy.ner import StanfordNERRunner
 from iepy.combined_ner import CombinedNERRunner
@@ -31,16 +31,10 @@ def media_wiki_to_txt(doc):
         doc.save()
 
 
-MEDIC_ENTITIES = ['DISEASE', 'SYMPTOM', 'MEDICAL_TEST']
-
-
-class MedicNERRunner(LitTaggerRunner):
-
-    def __init__(self):
-        labels = MEDIC_ENTITIES
-        filenames = ['tvseries/disease.txt', 'tvseries/symptom.txt',
-                     'tvseries/diagnostic_test.txt']
-        super(MedicNERRunner, self).__init__(labels, filenames)
+FREEBASE_ENTITIES = ['DISEASE', 'SYMPTOM', 'MEDICAL_TEST']
+FREEBASE_FILES = ['examples/tvseries/disease.txt', 
+                  'examples/tvseries/symptom.txt',
+                  'examples/tvseries/diagnostic_test.txt']
 
 
 if __name__ == '__main__':
@@ -51,13 +45,15 @@ if __name__ == '__main__':
     opts = docopt(__doc__, version=0.1)
     connect(opts['<dbname>'])
     docs = DocumentManager()
-    set_custom_entity_kinds(zip(map(lambda x: x.lower(), MEDIC_ENTITIES),
-                                MEDIC_ENTITIES))
+    set_custom_entity_kinds(zip(map(lambda x: x.lower(), FREEBASE_ENTITIES),
+                                FREEBASE_ENTITIES))
     pipeline = PreProcessPipeline([
         media_wiki_to_txt,
         TokenizeSentencerRunner(),
         StanfordTaggerRunner(),
-        CombinedNERRunner(MedicNERRunner(), StanfordNERRunner()),
+        CombinedNERRunner(
+            LiteralNERRunner(FREEBASE_ENTITIES, FREEBASE_FILES), 
+            StanfordNERRunner()),
         SyntacticSegmenterRunner(),
     ], docs
     )
