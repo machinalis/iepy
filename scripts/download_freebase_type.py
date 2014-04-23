bb3bf0e8374a00808366df74baa6c81852c6c82b
Franco M. Luque <francolq@gmail.com>
2014-04-23 17:58:51 -0300

    New script to download Freebase types. To be used in the tutorial.

---
+++ scripts/download_freebase_type.py
@@ -0,0 +1,29 @@
+"""
+IEPY's Freebase type instance names downloader (to be used with the Literal NER).
+
+Usage:
+    download_freebase_type.py <freebase_type_name> <output_file> [options]
+    download_freebase_type.py -h | --help | --version
+
+Options:
+  -h --help             Show this screen
+  --version             Version number
+  --no-aliases          Exclude instance aliases (included by default)
+  --to-lower            Convert non acronyms to lowercase
+"""
+from docopt import docopt
+
+from iepy.literal_ner import download_freebase_type, to_lower_normalizer
+
+if __name__ == '__main__':
+    opts = docopt(__doc__, version=0.1)
+    freebase_type_name = opts['<freebase_type_name>']
+    output_file = opts['<output_file>']
+    aliases = not opts['--no-aliases']
+    to_lower = opts['--to-lower']
+
+    if to_lower:
+        normalizer = to_lower_normalizer
+    else:
+        normalizer = None
+    download_freebase_type(freebase_type_name, output_file, normalizer, aliases)
