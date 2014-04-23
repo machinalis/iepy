ad0dfc6a6883f760e6ebb3b61e65bc1588a9ea15
Rafael Carrascosa <rcarrascosa@machinalis.com>
2014-04-23 13:36:05 -0300

    Modified and added tests for no classifier is trained if there's not enough data

--- tests/test_core.py
+++ tests/test_core.py
@@ -77,23 +77,29 @@ class TestFactExtractionInterface(unittest.TestCase):
             segment__tokens=tokens)
         return ev
 
-    def build_knowledge(self, relations_dict):
+    def build_training_knowledge(self, relations_dict):
         k = Knowledge()
         for relation_name, number in relations_dict.items():
             for i in range(number):
                 ev = self.get_evidence(relation_name)
-                k[ev] = 0.5
+                k[ev] = (i % 2 == 0)
         return k
 
     def test_one_fact_extractor_built_per_relation_in_available_data(self):
         b = BootstrappedIEPipeline(mock.MagicMock(), [])
-        kn = self.build_knowledge({'likes': 3, 'hates': 2})
+        kn = self.build_training_knowledge({'likes': 3, 'hates': 2})
         result = b.learn_fact_extractors(kn)
         self.assertEqual(len(result), 2)
 
+    def test_no_fact_extractor_is_built_when_not_enough_data(self):
+        b = BootstrappedIEPipeline(mock.MagicMock(), [])
+        kn = self.build_training_knowledge({'likes': 1, 'hates': 1})
+        result = b.learn_fact_extractors(kn)
+        self.assertEqual(len(result), 0)
+
     def test_fact_extractor_is_created_with_FactExtractorFactory(self):
         b = BootstrappedIEPipeline(mock.MagicMock(), [])
-        kn = self.build_knowledge({'likes': 3, 'hates': 2})
+        kn = self.build_training_knowledge({'likes': 3, 'hates': 2})
         with mock.patch('iepy.core.FactExtractorFactory') as m_FEF:
             b.learn_fact_extractors(kn)
         self.assertEqual(m_FEF.call_count, 2)
@@ -107,7 +113,7 @@ class TestFactExtractionInterface(unittest.TestCase):
     def test_returned_fact_extractor_has_method_predict(self):
         # ie, can be used for scoring an evidence
         b = BootstrappedIEPipeline(mock.MagicMock(), [])
-        kn = self.build_knowledge({'likes': 3})
+        kn = self.build_training_knowledge({'likes': 3})
         result = b.learn_fact_extractors(kn)
         predictor = list(result.values())[0]
         self.assertTrue(hasattr(predictor, 'predict'))
