f765cad3e7ec3403a19afa2373c70579e2ea1dd9
Rafael Carrascosa <rcarrascosa@machinalis.com>
2014-04-25 18:49:31 -0300

    Added mocks for readthedocs.org

--- docs/conf.py
+++ docs/conf.py
@@ -240,3 +240,36 @@ texinfo_documents = [
 
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
 #texinfo_show_urls = 'footnote'
+
+
+### Mocks for readthedocs.org
+# See http://read-the-docs.readthedocs.org/en/latest/faq.html#i-get-import-errors-on-libraries-that-depend-on-c-modules
+
+import sys
+
+
+class Mock(object):
+
+    __all__ = []
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+
+MOCK_MODULES = ['colorama']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
