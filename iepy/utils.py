5e9d8b56082d8b83b5532e96c843cbaa8b42d384
Javier Mansilla <jmansilla@machinalis.com>
2014-05-06 17:39:57 -0300

    tiny docstring change

--- iepy/utils.py
+++ iepy/utils.py
@@ -12,7 +12,7 @@ DIRS = AppDirs('iepy', getuser())
 def unzip(zipped_list, n):
     """returns n lists with the elems of zipped_list unsplitted.
     The general case could be solved with zip(*zipped_list), but here we
-    are dealing with:
+    are also dealing with:
       - un-zipping empy list to n empty lists
       - ensuring that all zipped items in zipped_list have lenght n, raising
         ValueError if not.
