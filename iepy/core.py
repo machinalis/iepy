84a0ef2941fdd6267ac09e8fe5ded198e0c9e924
Franco M. Luque <francolq@famaf.unc.edu.ar>
2014-05-22 15:03:51 -0300

    For uniformity, filter_evidence always returns scores in [1, 0], never booleans.

--- iepy/core.py
+++ iepy/core.py
@@ -257,7 +257,7 @@ class BootstrappedIEPipeline(object):
         evidence = Knowledge(self.answers)
         n = len(evidence)
         evidence.update(
-            (e, score > 0.5)
+            (e, score > 0.5 and 1 or 0)
             for e, score in self.evidence.items()
             if certainty(score) > self.evidence_threshold and e not in self.answers
         )
