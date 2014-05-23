850785479883ef5c329ab7d500c03ab5d9a0420c
Javier Mansilla <jmansilla@machinalis.com>
2014-05-23 16:24:44 -0300

    added iepyloop experimentation

---
+++ experimentation/loop/config_round1.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+u"""
+Bootstrap Experimental evaluation round 1.
+
+Usage:
+    config_round1.py <testdata.csv> <dbname>
+
+Options:
+ -h --help              Show this screen.
+
+The testdata.csv argument is a csv file containing the gold standard to be used
+to evaluate the different configurations. The absolute path and the md5 of the
+file will be added to the configurations.
+
+"""
+import os
+import hashlib
+
+from utils import apply_dict_combinations, check_configs
+
+# Proposing as unique candidate here the champion of the
+# experimentation/classifier/config_round3.py
+candidate_classifiers = [
+    {
+        u'classifier': u'svm',
+        u'classifier_args': {'gamma': 0.0001},
+        u'dimensionality_reduction': None,
+        u'dimensionality_reduction_dimension': None,
+        u'feature_selection': None,
+        u'feature_selection_dimension': None,
+        u'features': [
+            u'BagOfVerbLemmas False',
+            u'BagOfVerbLemmas True',
+            u'BagOfVerbStems False',
+            u'BagOfVerbStems True',
+            u'bag_of_pos',
+            u'bag_of_pos_in_between',
+            u'bag_of_word_bigrams',
+            u'bag_of_word_bigrams_in_between',
+            u'bag_of_wordpos',
+            u'bag_of_wordpos_bigrams',
+            u'bag_of_wordpos_bigrams_in_between',
+            u'bag_of_wordpos_in_between',
+            u'bag_of_words',
+            u'bag_of_words_in_between',
+            u'entity_distance',
+            u'entity_order',
+            u'in_same_sentence',
+            u'number_of_tokens',
+            u'other_entities_in_between',
+            u'symbols_in_between',
+            u'total_number_of_entities',
+            u'verbs_count',
+            u'verbs_count_in_between'],
+        u'scaler': True,
+        u'sparse': False},
+]
+
+
+def iter_configs(input_file_path, dbname):
+    input_file_path = os.path.abspath(input_file_path)
+    hasher = hashlib.md5(open(input_file_path, 'rb').read())
+    base = {
+        # Experiment configuration
+        u'experiment': u'bootstrap',
+        u'config_version': u'1',
+        u'data_shuffle_seed': "a-ha",
+        u'input_file_path': input_file_path,
+        u'input_file_md5': hasher.hexdigest(),
+        u'database_name': dbname,
+
+        # Human In The Middle configuration
+        u'answers_per_round': 5,
+        u'max_number_of_rounds': 5,
+
+        # Bootstrap configuration
+        u'prediction_config': {
+            u'method': u'predic',
+            u'scale_to_range': [0.1, 0.9]
+        },
+        u'fact_threshold': 0.89,
+        u'evidence_threshold': 0.89,
+        u'questions_sorting': 'score',
+        u'seed_facts_size': {
+            u'number_to_use': 5,
+            u'shuffle': "it's a trap"
+        },
+
+        # Classifier configuration
+        u'classifier_config': {}  # to be filled with each candidate-classifier
+    }
+
+    prediction_patch = {
+        u'method': [u'predic', u'predict_proba'],
+        u'scale_to_range': [None, [0.1, 0.9]]
+    }
+    prediction_range = list(
+        apply_dict_combinations(base['prediction_config'], prediction_patch)
+    )
+
+    patch = {
+        u'answers_per_round': [5, 15, 25],
+        u'max_number_of_rounds': [10, 15],
+        u'prediction_config': prediction_range,
+        u'fact_threshold': [0.89, 0.85],
+        u'evidence_threshold': [0.89, 0.85],
+        u'questions_sorting': [u'score', u'certainty'],
+    }
+
+    for classifier_config in candidate_classifiers:
+        base[u'classifier_config'] = classifier_config
+        for config in apply_dict_combinations(base, patch):
+            yield config
+
+
+if __name__ == '__main__':
+    import json
+    import sys
+    import logging
+
+    from docopt import docopt
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    opts = docopt(__doc__)
+    configs = list(iter_configs(opts[u'<testdata.csv>'], opts[u'<dbname>']))
+    check_configs(configs)
+    json.dump(configs, sys.stdout, sort_keys=True, indent=4,
+              separators=(u',', u': '))
---
+++ experimentation/loop/experiment_runner.py
@@ -0,0 +1,174 @@
+"""
+Experiment definition for fine tuning the whole IEPY looping
+"""
+import hashlib
+import random
+import time
+
+from featureforge.experimentation import runner
+
+from iepy.fact_extractor import FactExtractorFactory
+from iepy.knowledge import Knowledge
+import iepy.db
+from iepy.pycompatibility import PY2
+
+
+#
+# The class below is one way (an ugly one) to solve the issue of getting the
+# data file path into train_and_evaluate_classifier without having it in
+# the booked configuration.
+# There are other ways to do it, this one I hated less.
+#
+
+class Runner(object):
+    def __init__(self):
+        self.last_dbname = None
+        self.last_path = None
+        self.last_hash = None
+
+    def run_iepy(self, config):
+        assert self.config == config
+        assert "input_file_path" not in config
+        assert "database_name" not in config
+
+        if u"class_weight" in config[u"classifier_args"]:
+            d = config[u"classifier_args"][u"class_weight"]
+            assert "true" in d and "false" in d and len(d) == 2
+            config[u"classifier_args"][u"class_weight"] = {True: d["true"],
+                                                           False: d["false"]}
+        config = _fix_config(config)
+
+        # Prepare data
+        data = self.get_data(config)
+        train, test = self.get_train_test_indexes(config, len(data))
+        train = dict(data[i] for i in train)
+        test_evidences = (data[i][0] for i in test)
+
+        result = {
+            "train_size": len(train),
+            "test_size": len(test),
+            "dataset_size": len(data),
+            "start_time": time.time(),
+        }
+
+        # Train
+        extractor = FactExtractorFactory(config, train)
+
+        # Evaluate
+        correct = []
+        incorrect = []
+        tp, fp, tn, fn = 0.0, 0.0, 0.0, 0.0
+        predicted_labels = extractor.predict(test_evidences)
+        for i, predicted_label in zip(test, predicted_labels):
+            evidence, real_label = data[i]
+            if real_label == predicted_label:
+                correct.append(i)
+                if real_label:
+                    tp += 1
+                else:
+                    tn += 1
+            else:
+                incorrect.append(i)
+                if predicted_label:
+                    fp += 1
+                else:
+                    fn += 1
+
+        # Make stats
+        try:
+            precision = tp / (tp + fp)
+        except ZeroDivisionError:
+            precision = 1.0
+        try:
+            recall = tp / (tp + fn)
+        except ZeroDivisionError:
+            recall = 1.0
+        try:
+            f1 = 2 * (precision * recall) / (precision + recall)
+        except ZeroDivisionError:
+            f1 = 0.0
+        result.update({
+            "true_positives": tp,
+            "false_positives": fp,
+            "true_negatives": tn,
+            "false_negatives": fn,
+            "accuracy": (tp + tn) / len(data),
+            "precision": precision,
+            "recall": recall,
+            "f1": f1,
+            "correctly_predicted": correct,
+            "incorrectly_predicted": incorrect,
+            "end_time": time.time()
+        })
+        return result
+
+    def get_data(self, config):
+        if self.last_dbname != self.dbname or self.last_path != self.path or \
+           self.last_hash != config[u"input_file_md5"]:
+            iepy.db.connect(self.dbname)
+            data = sorted(Knowledge.load_from_csv(self.path).items())
+            self.data = data
+            self.last_dbname = self.dbname
+            self.last_path = self.path
+            hasher = hashlib.md5(open(self.path, "rb").read())
+            self.last_hash = hasher.hexdigest()
+            if self.last_hash != config[u"input_file_md5"]:
+                raise ValueError("Configured input file and actual input "
+                                 "file have different MD5 checksums")
+        return self.data
+
+    def get_train_test_indexes(self, config, N):
+        r = random.Random()
+        r.seed(config[u"data_shuffle_seed"])
+        indexes = list(range(N))
+        r.shuffle(indexes)
+        n = int(config[u"train_percentage"] * N)
+        return indexes[:n], indexes[n:]
+
+    def extender(self, config):
+        config[u'classifier_config']["features"] = set(config["features"])
+        self.path = config["input_file_path"]
+        self.dbname = config["database_name"]
+        del config["input_file_path"]
+        del config["database_name"]
+        self.config = config
+
+        # Add a database id/hash
+        iepy.db.connect(self.dbname)
+        dbhash = (iepy.db.TextSegment.objects.count(),
+                  iepy.db.IEDocument.objects.count(),
+                  iepy.db.Entity.objects.count())
+        config["database_hash"] = dbhash
+        return config
+
+
+def _fix_config(config):
+    """
+    In python 2 is necesary to change config keys and values to be str and not
+    unicode because some parts of scikit-learn will complain.
+    """
+    if PY2:
+        for d in [config, config[u"classifier_args"]]:
+            for key, value in list(d.items()):
+                del d[key]
+                key = str(key)
+                if isinstance(value, unicode):
+                    value = str(value)
+                d[key] = value
+    return config
+
+
+if __name__ == '__main__':
+    import os.path
+    import logging
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format=u"%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+    path = os.path.abspath(os.path.dirname(__file__))
+
+    r = Runner()
+    runner.main(r.run_iepy, r.extender,
+                booking_duration=60 * 15,  # 15 minutes
+                use_git_info_from_path=path)
