f9477ad8459467473ae2fcdedf947c79b91a0ba7
Javier Mansilla <jmansilla@machinalis.com>
2014-05-27 11:48:51 -0300

    Defined ExperimentalPipelineRunner

--- experimentation/loop/experiment_runner.py
+++ experimentation/loop/experiment_runner.py
@@ -7,11 +7,10 @@ import time
 
 from featureforge.experimentation import runner
 
-from iepy.fact_extractor import FactExtractorFactory
 from iepy.knowledge import Knowledge
 import iepy.db
 from iepy.pycompatibility import PY2
-
+from iepy.core import BootstrappedIEPipeline
 
 #
 # The class below is one way (an ugly one) to solve the issue of getting the
@@ -19,6 +18,43 @@ from iepy.pycompatibility import PY2
 # the booked configuration.
 # There are other ways to do it, this one I hated less.
 #
+
+
+class ReferenceProxyBootstrappedIEPipeline(BootstrappedIEPipeline):
+    """Wraps the classic pipeline, providing:
+        - automatic question answering, replacing Human In The Loop (uses
+          the reference corpus)
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.rounds = kwargs.pop('rounds')
+        self.answers_per_round = kwargs.pop('answers_per_round')
+        super(ReferenceProxyBootstrappedIEPipeline, self).__init__(*args, **kwargs)
+
+    def run_experiment(self):
+        self.start()  # blocking
+        answers_given = []
+        keep_looping = True
+        round_nr = 1
+        while keep_looping and round_nr <= self.rounds:
+            round_nr += 1
+            questions = list(self.questions_available())
+            if not questions:
+                keep_looping = False
+
+            answered = 0
+            for evidence, rank in questions:
+                reference_answer = self.gold_standard.get(evidence, None)
+                answers_given.append(reference_answer)
+                if reference_answer in [1.0, 0.0]:
+                    self.add_answer(evidence, reference_answer)
+                    answered += 1
+                if answered == self.answers_per_round:
+                    # enough answering for this round
+                    break
+            self.force_process()  # blocking
+        return answers_given
+
 
 class Runner(object):
     def __init__(self):
@@ -31,28 +67,33 @@ class Runner(object):
         assert "input_file_path" not in config
         assert "database_name" not in config
 
-        if u"class_weight" in config[u"classifier_args"]:
-            d = config[u"classifier_args"][u"class_weight"]
-            assert "true" in d and "false" in d and len(d) == 2
-            config[u"classifier_args"][u"class_weight"] = {True: d["true"],
-                                                           False: d["false"]}
         config = _fix_config(config)
 
         # Prepare data
         data = self.get_data(config)
-        train, test = self.get_train_test_indexes(config, len(data))
-        train = dict(data[i] for i in train)
-        test_evidences = (data[i][0] for i in test)
+        db_con = iepy.db.connect(self.dbname)
+        seed_facts = self.build_seed_facts(data, config)
 
         result = {
-            "train_size": len(train),
-            "test_size": len(test),
+            "seed_facts": seed_facts,
             "dataset_size": len(data),
             "start_time": time.time(),
         }
 
         # Train
-        extractor = FactExtractorFactory(config, train)
+        iepyloop = ReferenceProxyBootstrappedIEPipeline(
+            db_connector=db_con, seed_facts=seed_facts, gold_standard=data,
+            rounds=config[u'max_number_of_rounds'],
+            answers_per_round=config[u'answers_per_round'],
+            extractor_config=config[u'classifier_config'],
+            prediction_config=config[u'prediction_config'],
+            evidence_threshold=config[u'evidence_threshold'],
+            fact_threshold=config[u'fact_threshold'],
+            sort_questions_by=config[u'questions_sorting']
+        )
+
+        answers_given = iepyloop.run_experiment()
+        result[u'answers_given'] = answers_given
 
         # Evaluate
         correct = []
@@ -106,8 +147,7 @@ class Runner(object):
         if self.last_dbname != self.dbname or self.last_path != self.path or \
            self.last_hash != config[u"input_file_md5"]:
             iepy.db.connect(self.dbname)
-            data = sorted(Knowledge.load_from_csv(self.path).items())
-            self.data = data
+            self.data = Knowledge.load_from_csv(self.path)
             self.last_dbname = self.dbname
             self.last_path = self.path
             hasher = hashlib.md5(open(self.path, "rb").read())
@@ -117,16 +157,19 @@ class Runner(object):
                                  "file have different MD5 checksums")
         return self.data
 
-    def get_train_test_indexes(self, config, N):
+    def build_seed_facts(self, data, config):
+        seeds_info = config[u'seed_facts']
+        possible_seeds = []
+        for ev, s in data.items():
+            if s != 1.0 or ev.fact in possible_seeds:
+                continue
+            possible_seeds.append(ev.fact)
         r = random.Random()
-        r.seed(config[u"data_shuffle_seed"])
-        indexes = list(range(N))
-        r.shuffle(indexes)
-        n = int(config[u"train_percentage"] * N)
-        return indexes[:n], indexes[n:]
+        r.seed(seeds_info[u'shuffle'])
+        return r.sample(possible_seeds, seeds_info[u'number_to_use'])
 
     def extender(self, config):
-        config[u'classifier_config']["features"] = set(config["features"])
+        config[u'classifier_config']["features"] = set(config[u'classifier_config']["features"])
         self.path = config["input_file_path"]
         self.dbname = config["database_name"]
         del config["input_file_path"]
