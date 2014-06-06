52b8d3a50089c779c027fe7173152ce4080a3ad4
Javier Mansilla <jmansilla@machinalis.com>
2014-06-06 16:52:56 -0300

    creating user config dir if needed before starting downdloads

--- scripts/download_third_party_data.py
+++ scripts/download_third_party_data.py
@@ -1,9 +1,21 @@
+import errno
+import os
+
 import nltk.data
 from iepy.tagger import download as download_tagger
 from iepy.ner import download as download_ner
+from iepy.utils import DIRS
 
 
 def download_third_party_data():
+    print("Making sure configuration folder exists")
+    try:
+        os.makedirs(DIRS.user_data_dir)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(DIRS.user_data_dir):
+            pass
+        else:
+            raise
     print("Downloading punkt tokenizer")
     nltk.download("punkt")
     print("Downloading wordnet")
