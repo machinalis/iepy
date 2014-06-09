52aa84baa1acb04e557af3582dbb041dd6d48311
Javier Mansilla <jmansilla@machinalis.com>
2014-06-09 16:55:58 -0300

    wrongly patched before, hopefully correctly now

--- experimentation/loop/config_round6.py
+++ experimentation/loop/config_round6.py
@@ -196,11 +196,11 @@ def iter_configs(input_file_path, dbname):
             u'seed_facts': seed_options_range
         }
         for config in apply_dict_combinations(champ, patch):
+            if config['prediction_config']['method'] == u'predict_proba':
+                config['classifier_config']['classifier_args'][u'probability'] = True
             yield config
             config = deepcopy(config)
             config['classifier_config']['feature_selection_dimension'] = 10
-            if config['prediction_config']['method'] == u'predict_proba':
-                config['classifier_config']['classifier_args'][u'probability'] = True
             yield config
 
 
