2d9c1fca673206a6c77d32889683747b5b45e12e
Javier Mansilla <jmansilla@machinalis.com>
2014-09-10 13:13:16 -0300

    moving web ui folder (django project) inside iepy

--- iepy/webui/webui/settings.py
+++ iepy/webui/webui/settings.py
@@ -10,8 +10,11 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+if BASE_DIR not in sys.path:
+    sys.path.append(BASE_DIR)  # so apps are importable when running scripts
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -60,7 +63,7 @@ WSGI_APPLICATION = 'webui.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join('/', 'tmp', 'db.sqlite3'),
     }
 }
 
