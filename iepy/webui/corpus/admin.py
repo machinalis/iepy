2da19800b6230bb1dd1c34ec67208b3f7c47e6ec
Javier Mansilla <jmansilla@machinalis.com>
2014-09-17 16:03:38 -0300

    added django-admin definition for relations

--- iepy/webui/corpus/admin.py
+++ iepy/webui/corpus/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from corpus.models import IEDocument, Entity, EntityKind
+from corpus.models import IEDocument, Entity, EntityKind, Relation
 
 
 class EntityKindAdmin(admin.ModelAdmin):
@@ -17,3 +17,11 @@ class IEDocumentAdmin(admin.ModelAdmin):
     list_display = ['id', 'human_identifier', 'title']
     list_per_page = 20
 admin.site.register(IEDocument, IEDocumentAdmin)
+
+
+class RelationAdmin(admin.ModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # editing an existing object
+            return self.readonly_fields + ('left_entity_kind', 'right_entity_kind')
+        return self.readonly_fields
+admin.site.register(Relation, RelationAdmin)
