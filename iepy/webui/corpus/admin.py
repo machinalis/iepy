6782d7a8751266c02bf2c8af7b026bc9cf87b58c
Javier Mansilla <jmansilla@machinalis.com>
2014-09-18 11:45:08 -0300

    simple view and links created

--- iepy/webui/corpus/admin.py
+++ iepy/webui/corpus/admin.py
@@ -1,27 +1,37 @@
 from django.contrib import admin
+from django.core import urlresolvers
 
 from corpus.models import IEDocument, Entity, EntityKind, Relation
 
 
+@admin.register(EntityKind)
 class EntityKindAdmin(admin.ModelAdmin):
     pass
-admin.site.register(EntityKind, EntityKindAdmin)
 
 
+@admin.register(Entity)
 class EntityAdmin(admin.ModelAdmin):
     list_per_page = 20
-admin.site.register(Entity, EntityAdmin)
 
 
+@admin.register(IEDocument)
 class IEDocumentAdmin(admin.ModelAdmin):
     list_display = ['id', 'human_identifier', 'title']
     list_per_page = 20
-admin.site.register(IEDocument, IEDocumentAdmin)
 
 
+@admin.register(Relation)
 class RelationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'left_entity_kind', 'right_entity_kind', 'link_to_label')
+
+    def link_to_label(self, obj):
+        return '<a href="{0}">Label evidence</a>'.format(
+            urlresolvers.reverse('corpus:start_labeling_evidence', args=(obj.id,))
+        )
+    link_to_label.short_description = 'Labeling'
+    link_to_label.allow_tags = True
+
     def get_readonly_fields(self, request, obj=None):
         if obj:  # editing an existing object
             return self.readonly_fields + ('left_entity_kind', 'right_entity_kind')
         return self.readonly_fields
-admin.site.register(Relation, RelationAdmin)
--- iepy/webui/webui/urls.py
+++ iepy/webui/webui/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^corpus/', include('corpus.urls', namespace='corpus')),
 )
