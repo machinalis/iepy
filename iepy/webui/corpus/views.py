3c006336fa0ad1767f976656d8188c1bd4198bb3
Javier Mansilla <jmansilla@machinalis.com>
2014-09-18 13:09:39 -0300

    start-labeling asks for segment to the relation, and decides what to do based on if the segment exists or not

--- iepy/webui/corpus/views.py
+++ iepy/webui/corpus/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import get_object_or_404, render_to_response
+from django.shortcuts import get_object_or_404, render_to_response, redirect
 
 from corpus.models import Relation
 
 
 def start_labeling_evidence(request, relation_id):
-    #relation = get_object_or_404(Relation, pk=relation_id)
-    #s = pick_segment()
-    #redirect(show_and_process_questions_for_segment(rea, s))
-    return render_to_response('label_evidence.html',
-                              {'title': 'hello world'})
+    relation = get_object_or_404(Relation, pk=relation_id)
+    segment = relation.get_next_segment_to_label()
+    if segment is None:
+        return render_to_response('message.html',
+                                  {'msg': 'There are no more evidence to label'})
+    return redirect('label_evidence_for_segment', segment.pk)
+    #return render_to_response('label_evidence.html',
+    #                          {'title': 'hello world'})
 
 
 #def show_and_process_questions_for_segment(request, relation_id, segment_id):
