1a1746cfc473620c429bf0f40ef773d21ebd6d0d
Javier Mansilla <jmansilla@machinalis.com>
2014-09-19 16:24:34 -0300

    added trailing backslashes to urls

--- iepy/webui/corpus/urls.py
+++ iepy/webui/corpus/urls.py
@@ -3,10 +3,10 @@ from corpus import views
 
 urlpatterns = patterns(
     '',
-    url(r'^start_labeling_evidence/(?P<relation_id>\d+)',
+    url(r'^start_labeling_evidence/(?P<relation_id>\d+)/',
         views.start_labeling_evidence,
         name='start_labeling_evidence'),
-    url(r'^label_evidence_for_segment/(?P<relation_id>\d+)/(?P<segment_id>\d+)',
+    url(r'^label_evidence_for_segment/(?P<relation_id>\d+)/(?P<segment_id>\d+)/',
         views.label_evidence_for_segment,
         name='label_evidence_for_segment'),
 )
