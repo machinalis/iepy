6e5317a7e461a94778ead488b07f490232f910ce
Javier Mansilla <jmansilla@machinalis.com>
2014-09-19 12:06:02 -0300

    sketching tests for get next segment

--- tests/test_relations.py
+++ tests/test_relations.py
@@ -1,4 +1,5 @@
-from .factories import RelationFactory, EntityKindFactory
+from .factories import (RelationFactory, EntityKindFactory, IEDocFactory, TextSegmentFactory,
+                        EntityOccurrenceFactory)
 from .manager_case import ManagerTestCase
 
 
@@ -9,3 +10,17 @@ class TestRelations(ManagerTestCase):
         new_ek = EntityKindFactory()
         r.left_entity_kind = new_ek
         self.assertRaises(ValueError, r.save)
+
+
+class TestReferenceBuilding(ManagerTestCase):
+
+    def setUp(self):
+        self.doc = IEDocFactory()
+        self.r = RelationFactory()
+
+    def create_occurrence(self, e, offset, end):
+        return EntityOccurrenceFactory(document=self.d, entity=e,
+                                       offset=offset, offset_end=end)
+
+    def test_segment_with_lowest_id_is_retrieved(self):
+        pass
