f2547f2efc202e8e72efc531367c854f17458bd8
Javier Mansilla <jmansilla@machinalis.com>
2014-09-22 18:06:10 -0300

    removed old hand-made view for labeling segments

--- iepy/webui/corpus/views.py
+++ iepy/webui/corpus/views.py
@@ -18,48 +18,6 @@ def start_labeling_evidence(request, relation_id):
     return redirect('corpus:label_evidence_for_segment', relation.pk, segment.pk)
 
 
-def label_evidence_for_segment(request, relation_id, segment_id):
-    segment = get_object_or_404(TextSegment, pk=segment_id)
-    relation = get_object_or_404(Relation, pk=relation_id)
-    segment.hydrate()
-
-    evidence_forms = []
-    for evidence in segment.get_labeled_evidences(relation):
-        form = forms.LabeledRelationEvidenceForm(instance=evidence)
-        evidence_forms.append(form)
-
-    form_select = """
-        <select id="id_label" name="label">
-        <option value="NO">No relation present</option>
-        <option value="YE">Yes, relation is present</option>
-        <option value="DK">Don't know if the relation is present</option>
-        <option value="SK" selected="selected">Skipped labeling of this evidence</option>
-        <option value="NS">Evidence is nonsense</option>
-        </select>
-    """
-
-    title = "You're gonna answer questions for segment " \
-            "'{0}' respect relation '{1}'".format(segment, relation)
-    context = {
-        "title": title,
-        "segment": segment,
-        "relation": relation,
-        "questions": [
-            {
-                "left_occurrence": {"name": "Embolism", "id": 1},
-                "right_occurrence": {"name": "Brain damage", "id": 2},
-                "options": form_select
-            },
-            {
-                "left_occurrence": {"name": "Baby", "id": 3},
-                "right_occurrence": {"name": "Brain damage", "id": 2},
-                "options": form_select
-            },
-        ]
-    }
-    return render(request, 'corpus/segment_questions.html', context)
-
-
 class LabelEvidenceOnSegmentView(ModelFormSetView):
     template_name = 'corpus/segment_questions.html'
     form_class = EvidenceForm
