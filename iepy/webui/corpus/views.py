a155cf347ac8f4a393d830fc45330944f20db680
Javier Mansilla <jmansilla@machinalis.com>
2014-09-24 16:21:01 -0300

    basic view for starting labeling by documents added

--- iepy/webui/corpus/views.py
+++ iepy/webui/corpus/views.py
@@ -16,7 +16,16 @@ def next_segment_to_label(request, relation_id):
     if segment is None:
         return render_to_response('message.html',
                                   {'msg': 'There are no more evidence to label'})
-    return redirect('corpus:next_segment_to_label', relation.pk, segment.pk)
+    return redirect('corpus:label_evidence_for_segment', relation.pk, segment.pk)
+
+
+def next_document_to_label(request, relation_id):
+    relation = get_object_or_404(Relation, pk=relation_id)
+    doc = relation.get_next_document_to_label()
+    if doc is None:
+        return render_to_response('message.html',
+                                  {'msg': 'There are no more evidence to label'})
+    return redirect('corpus:label_evidence_for_document', relation.pk, doc.pk)
 
 
 def navigate_labeled_segments(request, relation_id, segment_id, direction):
