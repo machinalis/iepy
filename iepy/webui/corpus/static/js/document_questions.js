d31fee00d6a8daaaed2d514ab51752d256f83983
Javier Mansilla <jmansilla@machinalis.com>
2014-09-25 14:57:55 -0300

    just linting

--- iepy/webui/corpus/static/js/document_questions.js
+++ iepy/webui/corpus/static/js/document_questions.js
@@ -1,77 +1,77 @@
-"use strict"
+"use strict";
 
-function QuestionsController($scope){
+function QuestionsController($scope) {
 
     // ### Attributes ###
 
     $scope.eo_selected = undefined; // Id of the selected occurrence
 
     // Publish into the scope the variables defined globally
-    $scope.eos = eos;
-    $scope.relations = relations;
-    $scope.forms = forms;
+    $scope.eos = window.eos;
+    $scope.relations = window.relations;
+    $scope.forms = window.forms;
 
     // ### Methods ###
 
     // Sets the value for selectable on all entity occurrences
-    $scope.set_selectables = function(value){
-        for(var i in eos){
-            eos[i].selectable = value;
+    $scope.set_selectables = function (value) {
+        for (var i in $scope.eos) {
+            $scope.eos[i].selectable = value;
         }
-    }
+    };
 
-    $scope.eo_click = function(id){
-        var eo = eos[id];
+    $scope.eo_click = function (id) {
+        var eo = $scope.eos[id];
 
         // Not selectable
-        if(!eo.selectable){ return; }
+        if (!eo.selectable) { return; }
 
-        if($scope.eo_selected === undefined){
+        if ($scope.eo_selected === undefined) {
             // Marking as selected
             eo.selected = eo.selected + 1;
             $scope.eo_first_click(id);
         } else {
             $scope.eo_second_click(id);
         }
-    }
+    };
 
 
-    $scope.eo_first_click = function(id){
+    $scope.eo_first_click = function (id) {
         $scope.eo_selected = id;
 
         $scope.set_selectables(false);
-        eos[id].selectable = true;
+        $scope.eos[id].selectable = true;
         // Calculate wich ones must be selectable
-        for(var i in relations){
-            var rel = relations[i];
+        for (var i in $scope.relations) {
+            var rel = $scope.relations[i];
             var rel_index = rel.relation.indexOf(id);
-            if(rel_index >= 0){
-                var eo_id = relations[i].relation[(rel_index + 1) % 2];
-                eos[eo_id].selectable = true;
+            if (rel_index >= 0) {
+                var eo_id = $scope.relations[i].relation[(rel_index + 1) % 2];
+                $scope.eos[eo_id].selectable = true;
             }
         }
-    }
+    };
 
-    $scope.eo_second_click = function(id){
-        if($scope.eo_selected == id){
+    $scope.eo_second_click = function (id) {
+        if ($scope.eo_selected === id) {
             // You're de-selecting the one you've just selected
-            eos[id].selected = eos[id].selected - 1;
-        } else{
-            eos[id].selected = eos[id].selected + 1;
+            $scope.eos[id].selected = $scope.eos[id].selected - 1;
+        } else {
+            $scope.eos[id].selected = $scope.eos[id].selected + 1;
             var eo_id1 = $scope.eo_selected;
             var eo_id2 = id;
 
-            for(var i in relations){
-                var rel = relations[i];
+            for (var i in $scope.relations) {
+                var rel = $scope.relations[i];
                 var eo_rel_index1 = rel.relation.indexOf(eo_id1);
                 var eo_rel_index2 = rel.relation.indexOf(eo_id2);
-                if(eo_rel_index1 >= 0 && eo_rel_index2 >= 0){
-                    var form = forms[i];
+                if (eo_rel_index1 >= 0 && eo_rel_index2 >= 0) {
+                    var form = $scope.forms[rel.form_id];
                     var value = form.value ? false : true;
                     form.value = value;
-                    if(!value){
-                        eos[eo_id1].selected = eos[eo_id1].selected - 2;
-                        eos[eo_id2].selected = eos[eo_id2].selected - 2;
+                    if (!value) {
+                        $scope.eos[eo_id1].selected = $scope.eos[eo_id1].selected - 2;
+                        $scope.eos[eo_id2].selected = $scope.eos[eo_id2].selected - 2;
                     }
 
                 }
@@ -80,5 +80,5 @@ function QuestionsController($scope){
 
         $scope.set_selectables(true);
         $scope.eo_selected = undefined;
-    }
+    };
 }
