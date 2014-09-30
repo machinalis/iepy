c308dd92dd5c24f5d1a36ee63dfbf1e60c0764e5
Javier Mansilla <jmansilla@machinalis.com>
2014-09-30 13:29:12 -0300

    using the created serializer for CRUD of segments, and also limiting the exposed fields

--- iepy/webui/corpus/api.py
+++ iepy/webui/corpus/api.py
@@ -4,7 +4,10 @@ from corpus.models import EntityOccurrence, TextSegment
 
 class EOCRUDView(NgCRUDView):
     model = EntityOccurrence
+    fields = ['offset', 'offset_end']
 
 
 class SegmentCRUDView(NgCRUDView):
+    serializer_name = 'hydrated_python'
     model = TextSegment
+    fields = ['offset', 'tokens']
