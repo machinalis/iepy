574334b25ec3fb6ebb45336dd5c946e2412ed337
Javier Mansilla <jmansilla@machinalis.com>
2014-09-30 16:49:25 -0300

    requiring login for CRUD

--- iepy/webui/corpus/api.py
+++ iepy/webui/corpus/api.py
@@ -1,10 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.forms.models import modelform_factory
+from django.utils.decorators import method_decorator
 
 from djangular.views.crud import NgCRUDView
 from corpus.models import EntityOccurrence, TextSegment
 
 
-class EOCRUDView(NgCRUDView):
+class LoginNgCrudView(NgCRUDView):
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+
+class EOCRUDView(LoginNgCrudView):
     model = EntityOccurrence
     fields = ['offset', 'offset_end']
 
@@ -15,7 +23,7 @@ class EOCRUDView(NgCRUDView):
         return modelform_factory(self.model, fields=self.fields[:])
 
 
-class SegmentCRUDView(NgCRUDView):
+class SegmentCRUDView(LoginNgCrudView):
     serializer_name = 'hydrated_python'
     model = TextSegment
     fields = ['offset', 'tokens']
