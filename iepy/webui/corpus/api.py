4fe4109cdc3546749eb2651f9631d38ce5121aec
Javier Mansilla <jmansilla@machinalis.com>
2014-09-30 15:44:36 -0300

    declaring only offset fields as editable for EOs thru CRUD

--- iepy/webui/corpus/api.py
+++ iepy/webui/corpus/api.py
@@ -1,3 +1,5 @@
+from django.forms.models import modelform_factory
+
 from djangular.views.crud import NgCRUDView
 from corpus.models import EntityOccurrence, TextSegment
 
@@ -5,6 +7,12 @@ from corpus.models import EntityOccurrence, TextSegment
 class EOCRUDView(NgCRUDView):
     model = EntityOccurrence
     fields = ['offset', 'offset_end']
+
+    def get_form_class(self):
+        """
+        Build ModelForm from model
+        """
+        return modelform_factory(self.model, fields=self.fields[:])
 
 
 class SegmentCRUDView(NgCRUDView):
