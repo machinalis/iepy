3160a9cbf3873adb4baa9b71e7997b1c99f3b1ef
Javier Mansilla <jmansilla@machinalis.com>
2014-10-10 18:20:54 -0300

    loading labels for evidence on the abstraction layer

--- iepy/data/db.py
+++ iepy/data/db.py
@@ -6,15 +6,14 @@ the chosen database engine and ORM and the IEPY core and tools.
 """
 
 from collections import namedtuple
-try:
-    from functools import lru_cache
-except:
-    from functools32 import lru_cache
+from functools import lru_cache
+import logging
 
 import iepy
 iepy.setup()
 
-from iepy.data.models import IEDocument, TextSegment, Entity, EntityKind, Relation
+from iepy.data.models import (
+    IEDocument, TextSegment, Entity, EntityKind, Relation, EvidenceLabel)
 from iepy.preprocess.pipeline import PreProcessSteps
 
 
@@ -22,6 +21,8 @@ IEPYDBConnector = namedtuple('IEPYDBConnector', 'segments documents')
 
 # Number of entities that will be cached on get_entity function.
 ENTITY_CACHE_SIZE = 20  # reasonable compromise
+
+logger = logging.getLogger(__name__)
 
 
 class DocumentManager(object):
@@ -126,4 +127,61 @@ class CandidateEvidenceManager(object):
             )
         return evidences
 
+    @classmethod
+    def labeled_candidates_for_relation(cls, relation, conflict_solver=None):
+        logger.info("Loading candidate evidence from database...")
+        candidates = {e: None for e in cls.candidates_for_relation()}
 
+        labels = EvidenceLabel.object.filter(evidence_candidate__relation=relation,
+                                             label__in=[EvidenceLabel.NORELATION,
+                                                        EvidenceLabel.YESRELATION,
+                                                        EvidenceLabel.NONSENSE])
+        for e in candidates:
+            # This is CRYING for a preformance refactor. Will make a DB-query per
+            # evidence, when could do only one query for all and handle it on memory.
+            # If runs slows, here there's place for improvement
+            answers = labels.filter(evidence_candidate=e)
+            if not answers:
+                continue
+            if len(answers) == 1:
+                lbl = answers[0].label
+            elif len(set([a.label for a in answers])) == 1:
+                # several answers, all the same. Just pick the first one
+                lbl = answers[0].label
+            elif conflict_solver:
+                preferred = conflict_solver(answers)
+                if preferred is None:
+                    # unsolvable conflict
+                    continue
+                lbl = preferred.label
+            else:
+                continue
+            # Ok, we have a choosen answer. Lets see if it's informative
+            if lbl.label == EvidenceLabel.NONSENSE:
+                # too bad, not informative
+                continue
+            elif lbl.label == EvidenceLabel.NORELATION:
+                candidates[e] = False
+            elif lbl.label == EvidenceLabel.YESRELATION:
+                candidates[e] = True
+        return candidates
+
+    @classmethod
+    def conflict_resolution_by_judge_name(cls, judges_order):
+        # Only consider answers for the given judges, prefering those of the judge listed
+        # first. Returns None if not found.
+        def solver(ev_labels):
+            # expects to be called only when len(ev_labels) > 1
+            ev_labels = [el for el in ev_labels if el.judge in judges_order]
+            if ev_labels:
+                ev_labels.sort(key=lambda el: judges_order.index(el.judge))
+                return ev_labels[0]
+            return None
+        return solver
+
+    @classmethod
+    def conflict_resolution_newest_wins(cls):
+        def solver(ev_labels):
+            # expects to be called only when len(ev_labels) > 1
+            return sorted(ev_labels[:], key=lambda el: el.modification_date)[0]
+        return solver
