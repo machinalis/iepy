35601d70a64794440907b6026ecb7eb6a0cc454a
Gonzalo Garcia Berrotaran <ggarcia@machinalis.com>
2014-10-22 16:48:51 -0300

    fixed entry point location

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'iepy=iepy.scripts.command_line:execute_from_command_line',
+            'iepy=iepy.instantiation.command_line:execute_from_command_line',
         ],
     },
 )
