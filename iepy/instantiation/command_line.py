dd71cf72fdf57722056d26205ba9f4851f7acd08
Gonzalo Garcia Berrotaran <ggarcia@machinalis.com>
2014-10-22 15:59:49 -0300

    added creation of settings file when creating an instance

--- iepy/instantiation/command_line.py
+++ iepy/instantiation/command_line.py
@@ -20,12 +20,15 @@ import shutil
 
 import nltk.data
 from docopt import docopt
+from django.utils.crypto import get_random_string
 
 from iepy import defaults
 from iepy.utils import DIRS
 from iepy.preprocess.tagger import download as download_tagger
 from iepy.preprocess.corenlp import download as download_corenlp
 from iepy.preprocess.ner.stanford import download as download_ner
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
 
 def execute_from_command_line(argv=None):
@@ -40,14 +43,11 @@ def execute_from_command_line(argv=None):
         print("Folder already exists")
         sys.exit(1)
 
-    iepy_base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    iepy_scripts_path = os.path.join(iepy_base_path, "scripts")
-
     files_to_copy = [
-        os.path.join(iepy_scripts_path, "csv_to_iepy.py"),
-        os.path.join(iepy_scripts_path, "preprocess.py"),
-        os.path.join(iepy_scripts_path, "iepy_runner.py"),
-        os.path.join(iepy_scripts_path, "iepy_rules_runner.py"),
+        os.path.join(THIS_FOLDER, "csv_to_iepy.py"),
+        os.path.join(THIS_FOLDER, "preprocess.py"),
+        os.path.join(THIS_FOLDER, "iepy_runner.py"),
+        os.path.join(THIS_FOLDER, "iepy_rules_runner.py"),
     ]
 
     # Create folders
@@ -72,6 +72,14 @@ def execute_from_command_line(argv=None):
     with open(extractor_config_filepath, "w") as filehandler:
         json.dump(defaults.extractor_config, filehandler, indent=4)
 
+    # Create the settings file
+    print("An sqlite database will be created for you to work with.")
+    database_name = input("Database name: ")
+    settings_filepath = os.path.join(folder_name, "settings.py")
+    settings_data = get_settings_string(database_name)
+    with open(settings_filepath, "w") as filehandler:
+        filehandler.write(settings_data)
+
 
 def download_third_party_data():
     print("Making sure configuration folder exists")
@@ -89,6 +97,22 @@ def download_third_party_data():
     download_tagger()
     download_ner()
     download_corenlp()
+
+
+def get_settings_string(database_name):
+    template_settings_filepath = os.path.join(THIS_FOLDER, "settings.py.template")
+    with open(template_settings_filepath) as filehandler:
+        settings_data = filehandler.read()
+
+    if not database_name.endswith(".sqlite"):
+        database_name += ".sqlite"
+
+    chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+    secret_key = get_random_string(50, chars)
+    settings_data = settings_data.replace("{SECRET_KEY}", secret_key)
+    settings_data = settings_data.replace("{DATABASE_NAME}", database_name)
+
+    return settings_data
 
 
 if __name__ == "__main__":
