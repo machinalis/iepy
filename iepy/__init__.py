c7b0334b6a8e22bc0c1b82ce4fb369bfdbf69d33
Gonzalo Garcia Berrotaran <ggarcia@machinalis.com>
2014-10-23 12:24:10 -0300

    setup now sets the settings with the name prefix

--- iepy/__init__.py
+++ iepy/__init__.py
@@ -1,10 +1,18 @@
 import os
+import sys
 import django
 from django.conf import settings
 
 
-def setup():
+def setup(path=None, settings_prefix=None):
     if not settings.configured:
-        if not os.getenv('DJANGO_SETTINGS_MODULE'):
-            os.environ['DJANGO_SETTINGS_MODULE'] = 'iepy.webui.webui.settings'
+        if path is None:
+            if not os.getenv('DJANGO_SETTINGS_MODULE'):
+                os.environ['DJANGO_SETTINGS_MODULE'] = 'iepy.webui.webui.settings'
+        else:
+            if settings_prefix is None:
+                settings_prefix = path.rsplit(os.sep, 1)[1]
+            sys.path.append(path) # add to the first
+            os.environ['DJANGO_SETTINGS_MODULE'] = "{}_settings".format(settings_prefix)
+
         django.setup()
