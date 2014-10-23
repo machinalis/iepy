146eed15f966f6d22809e27531752bc6e411227c
Javier Mansilla <jmansilla@machinalis.com>
2014-10-23 17:21:20 -0300

    removing staled code

--- iepy/data/__init__.py
+++ iepy/data/__init__.py
@@ -1,14 +1,1 @@
-# adjustment of the sys.path for been able to import ORM stuff while defining
-# models
-from __future__ import absolute_import
-import os
-import sys
-import iepy
-
-ORM_NEEDED_PATH = os.path.join(iepy.__path__[0], 'webui')
-
-if ORM_NEEDED_PATH not in sys.path:
-    print('Adding %s to the python path' % ORM_NEEDED_PATH)
-    sys.path.append(ORM_NEEDED_PATH)
-    iepy.setup()
 
