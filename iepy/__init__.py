36a4ad70e8111efbebcb7859b76b49458dd1ec92
Javier Mansilla <jmansilla@machinalis.com>
2014-10-31 18:25:10 -0300

    safe mode import added

--- iepy/__init__.py
+++ iepy/__init__.py
@@ -11,7 +11,7 @@ with open(fname, encoding='utf-8') as filehandler:
 del fname
 
 
-def setup(fuzzy_path=None, settings_prefix=None):
+def setup(fuzzy_path=None, settings_prefix=None, _safe_mode=False):
     # Prevent nosetests messing up with this
     if not isinstance(fuzzy_path, (type(None), str)):
         # nosetests is grabing this function because its named "setup"... .
@@ -27,7 +27,7 @@ def setup(fuzzy_path=None, settings_prefix=None):
             sys.path.append(path)  # add to py-path the first
             os.environ['DJANGO_SETTINGS_MODULE'] = "{}_settings".format(settings_prefix)
         django.setup()
-        if settings.IEPY_VERSION != __version__:
+        if not _safe_mode and settings.IEPY_VERSION != __version__:
             sys.exit(
                 'Instance version is {} and current IEPY installation is {}.\n'
                 'Run iepy --upgrade on the instance.'.format(settings.IEPY_VERSION,
@@ -39,19 +39,19 @@ def _actual_path(fuzzy_path):
     """
     Given the fuzzy_path path, walks-up until it finds a folder containing a
     iepy-instance, and that folder path is returned"""
-
-    def is_iepy_instance(folder_path):
+    def _is_iepy_instance(folder_path):
         folder_name = os.path.basename(folder_path)
         expected_file = os.path.join(
             folder_path,
             "{}_settings.py".format(folder_name)
         )
         return os.path.exists(expected_file)
+
     # first, make sure we are handling an absolute path
     original = fuzzy_path   # used for debug
     fuzzy_path = os.path.abspath(fuzzy_path)
     while True:
-        if is_iepy_instance(fuzzy_path):
+        if _is_iepy_instance(fuzzy_path):
             return fuzzy_path
         else:
             parent = os.path.dirname(fuzzy_path)
@@ -59,3 +59,5 @@ def _actual_path(fuzzy_path):
                 raise ValueError(
                     "There's no IEPY instance on the provided path {}".format(original))
             fuzzy_path = parent
+
+
