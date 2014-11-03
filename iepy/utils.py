502121bdf57fee1701647bcb67d51ff26599401b
Javier Mansilla <jmansilla@machinalis.com>
2014-11-03 13:09:53 -0300

    able to extract from tar.gz bundles

--- iepy/utils.py
+++ iepy/utils.py
@@ -4,6 +4,7 @@ import gzip
 import logging
 import os
 import sys
+import tarfile
 import zipfile
 
 import wget
@@ -35,19 +36,23 @@ def unzip(zipped_list, n):
 
 
 def unzip_from_url(zip_url, extraction_base_path):
-    zipfile = None
+    got_zipfile = None
     try:
-        zipfile = wget.download(zip_url)
+        got_zipfile = wget.download(zip_url)
         print('')  # just because wget progress-bar finishes a line with no EOL
-        unzip_file(zipfile, extraction_base_path)
+        unzip_file(got_zipfile, extraction_base_path)
     finally:
         if zipfile:
-            os.remove(zipfile)
+            os.remove(got_zipfile)
 
 
 def unzip_file(zip_path, extraction_base_path):
-    zfile = zipfile.ZipFile(zip_path)
-    zfile.extractall(extraction_base_path)
+    if zip_path.endswith('.tar.gz'):
+        with tarfile.open(zip_path, mode='r:gz') as tfile:
+            tfile.extractall(extraction_base_path)
+    else:
+        zfile = zipfile.ZipFile(zip_path)
+        zfile.extractall(extraction_base_path)
 
 
 def make_feature_list(text):
