f66e27c064129f75ae93a84d471a11c446d6dbbf
Gonzalo Garcia Berrotaran <ggarcia@machinalis.com>
2014-11-04 16:37:07 -0300

    added parameter for relation name (on relation extractor)

--- experimentation/relationextractor_round2.py
+++ experimentation/relationextractor_round2.py
@@ -3,7 +3,7 @@
 Experimental evaluation of FactExtractor round 2.
 
 Usage:
-    factextractor_round2.py
+    factextractor_round2.py <relation_name>
 
 Options:
  -h --help              Show this screen.
@@ -12,7 +12,7 @@ from utils import apply_dict_combinations
 from iepy.utils import make_feature_list
 
 
-def iter_configs():
+def iter_configs(relation_name):
     base = {
         # Experiment configuration
         "config_version": "2relationextractionclassifier",
@@ -57,8 +57,10 @@ if __name__ == '__main__':
     from docopt import docopt
 
     logging.basicConfig(level=logging.DEBUG)
-    configs = list(iter_configs())
-
     opts = docopt(__doc__)
+    relation_name = opts["<relation_name>"]
+
+    configs = list(iter_configs(relation_name))
+
     json.dump(configs, sys.stdout, sort_keys=True, indent=4,
               separators=(u',', u': '))
