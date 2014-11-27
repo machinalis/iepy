793d85a05e745b6fa3f4e20160619785f8f02c2e
Javier Mansilla <jmansilla@machinalis.com>
2014-11-27 17:27:06 -0300

    replacing the migrations created while developing with a condensed one :-)

--- iepy/webui/corpus/migrations/0007_auto_20141127_2026.py
+++ iepy/webui/corpus/migrations/0007_auto_20141127_2026.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
@@ -14,13 +15,20 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='GazetteItem',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
                 ('text', models.CharField(unique=True, max_length=256)),
+                ('from_freebase', models.CharField(max_length=256)),
                 ('kind', models.ForeignKey(to='corpus.EntityKind')),
             ],
             options={
                 'abstract': False,
             },
             bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='entity',
+            name='gazette',
+            field=models.ForeignKey(blank=True, to='corpus.GazetteItem', null=True, on_delete=django.db.models.deletion.SET_NULL),
+            preserve_default=True,
         ),
     ]
