29648c2b1946222c6f710a1daaeea84f7298f8ee
Gonzalo Garcia Berrotaran <ggarcia@machinalis.com>
2015-02-27 14:08:12 -0300

    better performance on the metadata migration

--- iepy/webui/corpus/migrations/0014_data_migration_move_metadata.py
+++ iepy/webui/corpus/migrations/0014_data_migration_move_metadata.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import logging
 
 from django.db.models import Q
 from django.db import models, migrations
 
-import jsonfield.fields
+
+logging.basicConfig(format="%(asctime)-15s  %(message)s")
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.INFO)
 
 
 def move_metadata(apps, schema_editor):
@@ -14,14 +18,24 @@ def move_metadata(apps, schema_editor):
     documents = IEDocument.objects.filter(
         Q(metadata__isnull=False) | Q(title__isnull=False) | Q(url__isnull=False)
     )
-    for document in documents:
-        metadata = IEDocumentMetadata(
+    total = documents.count()
+    objects_to_create = []
+    for i, document in enumerate(documents.iterator()):
+        if i % 1000 == 0:
+            logger.info("Created {} out of {}".format(i, total))
+            IEDocumentMetadata.objects.bulk_create(objects_to_create)
+            objects_to_create = []
+
+        objects_to_create.append(IEDocumentMetadata(
             title=document.title,
             url=document.url,
             items=document.metadata,
             document=document
-        )
-        metadata.save()
+        ))
+
+    if objects_to_create:
+        logger.info("Created {} out of {}".format(i+1, total))
+        IEDocumentMetadata.objects.bulk_create(objects_to_create)
 
 
 class Migration(migrations.Migration):
