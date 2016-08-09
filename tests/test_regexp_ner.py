45cf82853be25ebc3d95e574b5eb1ada6f784246
Franco M. Luque <francolq@famaf.unc.edu.ar>
2016-08-09 09:32:18 -0300

    Test for regexps with named groups.

--- tests/test_regexp_ner.py
+++ tests/test_regexp_ner.py
@@ -1,4 +1,6 @@
-from iepy.preprocess.ner.regexp import RegExpNERRunner
+from unittest import TestCase
+
+from iepy.preprocess.ner.regexp import RegExpNERRunner, TokenSearcher
 
 from .factories import SentencedIEDocFactory
 from .manager_case import ManagerTestCase
@@ -23,3 +25,20 @@ class TestRegExpNERRunner(ManagerTestCase, NERTestMixin):
         # (the tokenizer splits can't in two parts)
         entities_triples = [(5, 7, 'MEDICAL_TEST'), (22, 23, 'MEDICAL_TEST')]
         self.check_ner_result(doc, entities_triples)
+
+
+class TestTokenSearcher(TestCase):
+
+    def test(self):
+        sent = "Chase notes she 's negative for HIV and Hepatitis C"
+        regexp = '<HIV>|<Hepatitis><[A-C]>'
+        searcher = TokenSearcher(sent.split())
+        matches = [(m.group(), m.span()) for m in searcher.finditer(regexp)]
+        self.assertEqual(matches, [(['HIV'], (6, 7)), (['Hepatitis', 'C'], (8, 10))])
+
+    def test_named_group(self):
+        sent = "Cuddy points out that the CT scan showed the patient has a metal pin in her arm and can 't undergo an MRI"
+        regexp = '(<an>|<the>) (?P<<name>> <[A-Z]+><scan>|<MRI>)'
+        searcher = TokenSearcher(sent.split())
+        matches = [(m.group('name'), m.span('name')) for m in searcher.finditer(regexp)]
+        self.assertEqual(matches, [(['CT', 'scan'], (5, 7)), (['MRI'], (22, 23))])
